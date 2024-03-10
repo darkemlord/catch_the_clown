08b628331311b4c95e6f5b393577c20eba65b496
emanuel caro <darkemlord@gmail.com>
2024-03-10 17:13:56 +0900

    refs: Game completed

--- main.py
+++ main.py
@@ -81,32 +81,32 @@ while running:
         if event.type == pg.QUIT:
             running = False
 
-    # Check if the click was made
-    if event.type == pg.MOUSEBUTTONDOWN:
-        mouse_x = event.pos[0]
-        mouse_y = event.pos[1]
+        # Check if the click was made
+        if event.type == pg.MOUSEBUTTONDOWN:
+            mouse_x = event.pos[0]
+            mouse_y = event.pos[1]
 
-        # The clown was clicked
-        if clown_rect.collidepoint(mouse_x, mouse_y):
-            click_sound.play()
-            clown_velocity += CLOWN_ACCELERATION
-            # Move the clown in a new direction
-            previous_dx = clown_dx
-            previous_dy = clown_dy
-            while previous_dx == clown_dx and previous_dy == clown_dy:
-                clown_dx = random.choice([-1, 1])
-                clown_dy = random.choice([-1, 1])
-        # We missed the clown
-        else:
-            miss_sound.play()
-            player_lives -= 1
+            # The clown was clicked
+            if clown_rect.collidepoint(mouse_x, mouse_y):
+                click_sound.play()
+                score += 1
+                clown_velocity += CLOWN_ACCELERATION
+                # Move the clown in a new direction
+                previous_dx = clown_dx
+                previous_dy = clown_dy
+                while previous_dx == clown_dx and previous_dy == clown_dy:
+                    clown_dx = random.choice([-1, 1])
+                    clown_dy = random.choice([-1, 1])
+            # We missed the clown
+            else:
+                miss_sound.play()
+                player_lives -= 1
 
     # Move the clown
     clown_rect.x += clown_dx * clown_velocity
     clown_rect.y += clown_dy * clown_velocity
 
     # Bounce the clown off edges of the display
-
     if clown_rect.left <= 0 or clown_rect.right >= WINDOW_WIDTH:
         clown_dx = -1 * clown_dx
     if clown_rect.top <= 0 or clown_rect.bottom >= WINDOW_HEIGHT:
@@ -114,7 +114,34 @@ while running:
 
     # Update HUD
     score_text = font.render("Score: " + str(score), True, YELLOW)
-    lives_text = font.render("Lives: " + str(lives_text), True, YELLOW)
+    lives_text = font.render("Lives: " + str(player_lives), True, YELLOW)
+
+    # Check game over
+    if player_lives == 0:
+        display_surface.blit(game_over_text, game_over_rect)
+        display_surface.blit(continue_text, continue_rect)
+        pg.display.update()
+
+        # Pause until the player clicks then reset game
+        pg.mixer.music.stop()
+        is_paused = True
+        while is_paused:
+            for event in pg.event.get():
+                # the player wants to play again
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    score = 0
+                    player_lives = PLAYER_STARTING_LIVES
+
+                    clown_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+                    clown_velocity = CLOWN_STARTING_VELOCITY
+                    clown_dx = random.choice([-1, 1])
+                    clown_dy = random.choice([-1, 1])
+
+                    pg.mixer.music.play(-1, 0.0)
+                if event.type == pg.QUIT:
+                    is_paused = False
+                    running = False
+
     # Fill surface and remove duplicated images background
     display_surface.blit(background_image, background_rect)
 
